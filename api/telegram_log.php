cc551f7390265ff8dc9bc59115eb15fa51094d3d
Clayton-Modder <cmempresa.ru@proton.me>
2025-12-28 13:49:03 -0300

    Update telegram_log.php

--- api/telegram_log.php
+++ api/telegram_log.php
@@ -1,6 +1,6 @@
 <?php
 $BOT_TOKEN = "8553413416:AAETpmhLL933bqSv__RsVtp9j8RJI1D3WjI";
-$CHAT_ID  = "-3621587880";
+$CHAT_ID  = "-5286992033";
 
 $canal = $_POST['canal'] ?? 'Desconhecido';
 $url   = $_POST['url'] ?? '';
